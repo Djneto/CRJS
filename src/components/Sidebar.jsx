845d2d0634b9407601e295e0cd2ac1acee5f574f
Djalma Farias Bastos Neto <df82835es@df.trf1.gov.br>
2023-07-06 15:34:08 -0300

    OK

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
 export function Sidebar() {
@@ -8,10 +9,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y29kZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://avatars.githubusercontent.com/u/46111926?v=4"
-        />
+        <Avatar src={'https://avatars.githubusercontent.com/u/46111926?v=4'} />
         <strong>Djalma Bastos</strong>
         <span>Full-Stack Developer</span>
       </div>
